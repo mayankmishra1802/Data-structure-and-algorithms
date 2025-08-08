26b5eb5625b564cfc7d786bd799742e98bdb6029
Mayank Mishra <mayankmishra1802@gmail.com>
2025-08-08 23:07:42 +0530

    added tortoise and hare algo

---
+++ 003-algorithms/011-tortoise-hare-algorithm.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+class node{ 
+    public:
+    int data;
+    node*next;
+    node(int d){ 
+        data=d;
+        next=NULL;
+    }
+};
+
+void PrintLL(node*head){
+    while(head!=NULL){
+        cout<<head->data<<"->";
+        head=head->next;
+    }
+    cout<<"NULL"<<endl;
+}
+
+void insert_at_end(node*&head,node*&tail,int data){
+    if(head==NULL){  //if LL is empty;
+        node*n= new node(data); 
+        head=tail=n;
+    }
+    else{
+        node*n= new node(data);
+        tail->next=n;
+        tail=n;
+    }
+}
+
+bool cycledetection(node*head){
+    if (head == NULL || head->next == NULL){
+        return false;
+    }
+    node*sp=head; //slow pointer
+    node*fp=head; //fast pointer
+    while(fp!=NULL && fp->next!=NULL){
+        sp=sp->next;
+        fp=fp->next->next;
+        if(sp==fp){  //cycle detected
+            return true;
+        }
+    }
+    return false;
+}
+int main()
+{
+    
+    node*head,*tail;
+    head=tail=NULL; 
+    insert_at_end(head,tail,1);
+    insert_at_end(head,tail,2);
+    insert_at_end(head,tail,3);
+    insert_at_end(head,tail,4);
+    insert_at_end(head,tail,5);
+    insert_at_end(head,tail,6);
+    insert_at_end(head,tail,7);
+    PrintLL(head); 
+    (cycledetection(head))?cout<<"yes":cout<<"no";
+    return 0;
+}
